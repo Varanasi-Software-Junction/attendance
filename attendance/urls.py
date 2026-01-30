ea11b1f396cc74b63aae063357a52043add94a54
Champak's World <champaksworld@gmail.com>
2026-01-30 12:29:41 +0530

    Added simple + and -

--- attendance/urls.py
+++ attendance/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.hello),
-    path("hi/", views.hi),
+    path("arithmetic/", views.arithmeticstart),
+    path("calculate/", views.calculate),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,11 +1,28 @@
-from  datetime import datetime
+from datetime import datetime
 from django.shortcuts import render, HttpResponse
+
+
 def hello(request):
-    
+
     return HttpResponse("Hello")
-def hi(request):
-    
+
+
+def arithmeticstart(request):
+
     # return HttpResponse("Hi")
-    return render(request,"hi.html")
+    return render(request, "arithmetic.html")
 
 
+def calculate(request):
+    a = int(request.GET["a"])
+    b = int(request.GET["b"])
+    cmd = request.GET["cmd"]
+    result = ""
+    if cmd == "Add":
+        result = a+b
+    elif cmd == "Sub":
+        result = a-b
+    print(a, b, cmd, result)
+
+    return HttpResponse("Calculate")
+    # return render(request,"arithmetic.html")
